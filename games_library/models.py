462f4284995f32ce5cc7b5ede0e8e5e78740f7f8
Temus <temus@wir.pl>
2022-02-02 11:58:19 +0100

    update unique field

--- games_library/models.py
+++ games_library/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class AppData(models.Model):
-    app_id = models.IntegerField()
+    app_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=100)
 
     def __str__(self):
