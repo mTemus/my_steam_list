9390106c6e3c8e05506f4698a9c52c4f745779b2
Temus <temus@wir.pl>
2022-02-02 11:03:30 +0100

    perform basic migrations

--- games_library/views.py
+++ games_library/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins
-from models import AppData
-from serializers import AppDataSerializer
+from .models import AppData
+from .serializers import AppDataSerializer
 
 # Create your views here.
 
 class AppDataGenericView(GenericViewSet, mixins.ListModelMixin):
     queryset = AppData.objects.all()
     serializer_class = AppDataSerializer
+
+    
